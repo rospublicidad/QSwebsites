bb22e01a4bc24b2a25ecf1a15974137f13258ec4
Juan Quino <quinojuan@gmail.com>
2023-04-26 10:16:26 -0300

    responsive1

---
+++ funciones.js
@@ -0,0 +1,11 @@
+// // Ejercicio 1
+// function saludo() {
+//     var nombre = prompt("Ingrese su nombre")
+//     var apellido = prompt("Ingrese su apellido")
+//     var edad = prompt("Ingrese su edad")
+//     console.log("Se llama: " + nombre + ", se apellida: " + apellido + " y tiene: " + edad + " años")
+// }
+
+// saludo()
+
+prompt("Ingrese su nombre")
